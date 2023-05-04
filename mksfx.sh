5ec41be0480d80751dfed0e0e1a65b7f4b5342cb
nf <nf@wh3rd.net>
2023-05-04 20:48:05 +1000

    better sfx

--- mksfx.sh
+++ mksfx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-files="freeze hold hold2 rot x"
+files="freeze hold rot x line tetris"
 
 for name in $files; do
 	sox ${name}.aif -t raw -c 1 -r 44100 -b 8 -e unsigned-integer --norm ${name}.raw
