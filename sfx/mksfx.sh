5b103e101993775d5c250720c519c272f3833362
nf <nf@wh3rd.net>
2023-05-06 11:10:24 +1000

    move sfx to dir

--- sfx/mksfx.sh
+++ sfx/mksfx.sh
@@ -6,5 +6,5 @@ for name in $files; do
 	sox ${name}.aif -t raw -c 1 -r 44100 -b 8 -e unsigned-integer --norm ${name}.raw
 done
 
-go run samples.go $(for n in $files; do echo -n "${n}.raw "; done) > sfx.tal 
+go run sfx.go $(for n in $files; do echo -n "${n}.raw "; done) > sfx.tal 
 
